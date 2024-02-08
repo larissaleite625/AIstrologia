f071986acdd3b08e8c3ae0affc3434e700f562c2
larissaleite625 <86159676+LarissaAthayde@users.noreply.github.com>
2024-02-07 22:53:33 -0300

    Sua mensagem de commit

---
+++ api_developer.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import tweepy
+import time
+
+
+
+api = tweepy.Client(
+    consumer_key = 'API KEY',
+    consumer_secret = 'API Key Secret',
+    access_token = 'Access Token',
+    access_token_secret= 'access token secret'
+)
+
+# Texto original que você quer tweetar, dividido por pontos
+
+with open('C:/Users/PC/Desktop/Projetos/AIstrologia/twitterzao.txt', 'r', encoding='utf-8') as file:
+    texto_original = file.read()
+
+
+mensagens = texto_original.split(".")
+
+# Iterar sobre cada mensagem, remover espaços vazios e tweetar se não estiver vazio
+for msg in mensagens:
+    if msg:  # Verifica se a mensagem não está vazia
+        try:
+            tweet = api.create_tweet(text=msg)
+            print(f"Tweet publicado: {tweet}")
+            time.sleep(10)  # Espera 10 segundos antes de publicar o próximo tweet
+        except Exception as e:
+            print(f"Erro ao publicar tweet: {e}")
+            print("Vai dormir")
